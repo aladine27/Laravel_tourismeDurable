9679313af3479554bf38e2dcbb310e5fcb46059e
alaedineibrahim <alaedineibrahim@gmail.com>
2024-10-16 12:42:32 +0100

    Update SideBar

--- resources/views/partials/dashboard/vertical-nav.blade.php
+++ resources/views/partials/dashboard/vertical-nav.blade.php
@@ -26,6 +26,26 @@
         </i>
         <span class="item-name">Nos Événements</span>
     </a>
+     <li class="nav-item">
+        <a class="nav-link {{ activeRoute(route('tours.index')) }}" href="{{ route('tours.index') }}">
+            <i class="icon">
+                <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" fill="currentColor"></path>
+                </svg>
+            </i>
+            <span class="item-name">Tours</span>
+        </a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link {{ activeRoute(route('guides.index')) }}" href="{{ route('guides.index') }}">
+            <i class="icon">
+                <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-3.31 0-10 1.67-10 5v2h20v-2c0-3.33-6.69-5-10-5z" fill="currentColor"></path>
+                </svg>
+            </i>
+            <span class="item-name">Guides</span>
+        </a>
+    </li>
     <li class="nav-item">
     <a class="nav-link {{ activeRoute(route('tickets.index')) }}" aria-current="page" href="{{ route('tickets.index') }}">
         <i class="icon">
