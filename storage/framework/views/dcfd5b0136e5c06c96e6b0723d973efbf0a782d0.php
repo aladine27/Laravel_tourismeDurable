8b5de61deef156167193ca3d60a0821ab2914e33
alaedineibrahim <alaedineibrahim@gmail.com>
2024-11-14 21:57:58 +0100

    fix bug

---
+++ storage/framework/views/dcfd5b0136e5c06c96e6b0723d973efbf0a782d0.php
@@ -0,0 +1,5 @@
+<div class="loader simple-loader">
+    <div class="loader-body"></div>
+</div>
+
+<?php /**PATH C:\Users\MSI\Desktop\5eme\Laravel\projet\Laravel_RescuFood\resources\views/partials/dashboard/_body_loader.blade.php ENDPATH**/ ?>
